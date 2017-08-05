64e8bbdb5df7c54f151850a29bb90b743357e4d2
dev-harsh1998 <harshit.india@linaro.org>
2017-08-05 21:30:47 +0530

    msm8916-common: add shoaib's tuning script and make it live long forever
    * Shoaib bhai has numerous unsung contributions to lenovo let it be testing to developing roms and helping me learn and fix bugs and he has an equal share in everything and is also one of the peak reason that this punny lenovo boots nougat and works well community cant payback his contributions in anyway so why not lets make a script in his name and make it stay forever :D viva la shoaib

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,6 +3,13 @@ LOCAL_PATH:= $(call my-dir)
 # Configuration scripts
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.shoaib.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.shoaib.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
---
+++ rootdir/etc/init.shoaib.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+echo 1 > /sys/module/process_reclaim/parameters/enable_process_reclaim
+echo 70 > /sys/module/process_reclaim/parameters/pressure_max
+echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
+# Kill Almk
+echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+# Do your left over job u script
+echo 50 > /sys/module/process_reclaim/parameters/pressure_min
+echo 512 > /sys/module/process_reclaim/parameters/per_swap_size
+echo "18432,23040,27648,32256,36864,46080" > /sys/module/lowmemorykiller/parameters/minfree
+echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+echo 2919 > /proc/sys/vm/min_free_kbytes
+echo 6075 > /proc/sys/vm/extra_free_kbytes
