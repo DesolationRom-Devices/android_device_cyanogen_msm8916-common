56b79c86255fd08d3fea6109191737fcf7bfa232
Harshit Jain <harshitjain6751@gmail.com>
2017-08-06 18:37:48 +0530

    .......
    git rebase -i HEAD~2
    fixup

--- product/init.mk
+++ product/init.mk
@@ -1,7 +1,7 @@
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.rc \
-    init.qcom.mem.sh \
+    init.shoaib.sh \
     init.qcom.power.rc \
     init.qcom.ssr.rc \
     init.recovery.qcom.rc \
